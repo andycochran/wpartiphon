ed6f3129191c6e5409328ac9c82a5de6780b3167
andycochran <andrewthomascochran@gmail.com>
2015-12-17 22:42:36 -0500

    fix mobie nav, enqueue minified scripts, hide empty paragraphs

--- wp-content/themes/artiphon/parts/nav-topbar.php
+++ wp-content/themes/artiphon/parts/nav-topbar.php
@@ -1,12 +1,13 @@
 <div class="top-bar" id="main-menu">
 	<div class="row">
 		<div class="columns">
+			<button id="mainMenuToggler" class="hide-for-medium float-right" type="button" data-toggle="mainMenu"><span class="show-for-sr">Menu</span></button>
 			<div class="top-bar-left">
 				<ul class="menu">
 					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a></li>
 				</ul>
 			</div>
-			<div class="top-bar-right">
+			<div id="mainMenu" class="top-bar-right show-for-medium" data-toggler=".show-for-medium">
 				<?php joints_top_nav(); ?>
 			</div>
 		</div>

834db460363682b3b8ee25c6ec5e21fe9b6f834c
andycochran <andrewthomascochran@gmail.com>
2016-02-17 00:31:54 -0500

    Make artiphon logo link to artiphon.com

--- wp-content/themes/artiphon/parts/nav-topbar.php
+++ wp-content/themes/artiphon/parts/nav-topbar.php
@@ -4,7 +4,8 @@
 			<button id="mainMenuToggler" class="hide-for-medium float-right" type="button" data-toggle="mainMenu"><span class="show-for-sr">Menu</span></button>
 			<div class="top-bar-left">
 				<ul class="menu">
-					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a></li>
+					<!-- <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a></li> -->
+					<li><a href="http://artiphon.com/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a></li>
 				</ul>
 			</div>
 			<div id="mainMenu" class="top-bar-right show-for-medium" data-toggler=".show-for-medium">

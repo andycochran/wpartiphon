1a006cb5149f92d19947c833910b60d474b45167
andycochran <andrewthomascochran@gmail.com>
2016-04-19 22:27:54 -0400

    link logo to /

--- wp-content/themes/artiphon-v2/parts/nav-globalbar.php
+++ wp-content/themes/artiphon-v2/parts/nav-globalbar.php
@@ -1,6 +1,5 @@
 <div class="row columns">
-  <a href="//artiphon.com/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a>
-  <!-- <a href="<?php echo home_url(); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a> -->
+  <a href="<?php echo home_url(); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo@2x.png" /><span class="show-for-sr"><?php bloginfo('name'); ?></span></a>
   <button class="menu-icon dark" type="button" data-toggle="main-menu"><span class="show-for-sr">Menu</span></button>
   <div class="dropdown-pane" id="main-menu" data-dropdown data-auto-focus="false" data-hover="true" data-hover-pane="true">
     <?php joints_top_nav(); ?>
